6ff403fa331ec29b87d6af2c7d0abd4d24e758ff
tangiki <tangiki@users.noreply.github.com>
2017-03-24 12:07:50 +1100

    Update header.h
    unify the variable name and add few comments

--- Main/header.h
+++ Main/header.h
@@ -3,6 +3,7 @@
 
 
 
+
 #define potPin A0 // select the input pin for the potentiometer A
 #define PWMA 2 //Speed control
 #define AIN1 24 //Direction
@@ -14,17 +15,17 @@
 #define CLOCKWISE 0
 #define COUNTER_CLOCKWISE 1
 #define MOTOR_A 1
-#define EPS 50
+#define EPS 3
 #define ONE_REV 1024
 #define EDGE_DETECTION 100
-#define TimeTolerance 100
+#define TIME_TOLERANCE 100
 int finish = 0;
-elapsedMillis ElapsedTime;
+elapsedMillis elapsedTime;
 
 //Define Variables we'll be connecting to
-double Setpoint, CurrentPosition, Output,rev;
-int multiplier,LastPot, PotVal;
+double setPoint, currentPosition, Output,rev;
+int multiplier,lastPot, potVal;
 
 //Specify the links and initial tuning parameters
 double Kp = 1, Ki = 0.1, Kd = 0;
-PID myPID(&CurrentPosition, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID myPID(&currentPosition, &Output, &setPoint, Kp, Ki, Kd, DIRECT);
