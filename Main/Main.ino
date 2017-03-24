14cddbfea8a0507f5a77a2f0cabbb718f6eff424
tangiki <tangiki@users.noreply.github.com>
2017-03-24 11:59:44 +1100

    Update Main.ino
    unify the variable name and add few comments

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,70 +1,34 @@
-#include "header.h"
-
-// the setup routine runs once when you press reset:
-void setup() {
-  // Initial serial com
-  Serial.begin(115200);
-
-  // Setting pin mode
-  pinMode(STBY, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-
-  //initialize the variables we're linked to
-  PotVal = analogRead(potPin);
-  Setpoint = 5000;
-  rev = 0;
-  CurrentPosition = PotVal;
-  LastPot = CurrentPosition;
-
-  myPID.SetSampleTime(3); // in ms
-  myPID.SetMode(AUTOMATIC); //turn the PID on
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-  PotVal = analogRead(potPin);    // read the value from the sensor
-
-  if (PotVal - LastPot < -EDGE_DETECTION && ElapsedTime > TimeTolerance) {
-    ElapsedTime = 0;
-    rev++;
+int action(int option) {
+  int val = 0;
+  switch (option) {
+    case 1: {
+        //do something when var equals 1
+        int finish = 0;
+        move(MOTOR_A, HALF_SPEED, CLOCKWISE);
+        while (finish = 0) {
+          val = analogRead(potPin);    // read the value from the sensor
+          Serial.println(val);
+          if (val > 500) {
+            stop();
+            finish = 1;
+          }
+        }
+        break;
+      }
+    case 2:
+      //do something when var equals 2
+      break;
+    case 3:
+      //do something when var equals 3
+      break;
+    case 4:
+      //do something when var equals 4
+      break;
+    default:
+      // if nothing else matches, do the default
+      // default is optional
+      break;
   }
-  if (PotVal - LastPot > EDGE_DETECTION && ElapsedTime > TimeTolerance) {
-    ElapsedTime = 0;
-    rev--;
-  }
-
-  CurrentPosition = PotVal + rev * ONE_REV;
-  if (abs(CurrentPosition - Setpoint) > 50 ) {
-    double Kp = 1, Ki = 0.1, Kd = 0;
-  } else {
-    double Kp = 50, Ki = 1, Kd = 0;
-  }
-  myPID.Compute();
-  Serial.print("Pot: ");
-  Serial.println(PotVal);
-  Serial.print("CurrentPosition: ");
-  Serial.println(CurrentPosition);
-  Serial.print("Output: ");
-  Serial.println(Output);
-  Serial.print("Rev: ");
-  Serial.println(rev);
-  Serial.println();
-  Serial.println();
-
-  //  Serial.println(myPID.GetITerm());
-  if (CurrentPosition < Setpoint - EPS) {
-    myPID.SetControllerDirection(DIRECT);
-    move(MOTOR_A, Output, CLOCKWISE);
-  }
-  else if (CurrentPosition > Setpoint + EPS) {
-    myPID.SetControllerDirection(REVERSE);
-    move(MOTOR_A, Output, COUNTER_CLOCKWISE);
-    Serial.println("Reverse ");
-  }
-  else {
-    stop();
-  }
-  LastPot = PotVal;
+  // return 1 if no error occurred
+  return 1;
 }
